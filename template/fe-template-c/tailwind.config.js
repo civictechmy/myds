01d1f8d3165046e6d387c1bc82b3bd9bab078376
irfanismail <irfan.ismail96@gmail.com>
2025-11-18 13:04:42 +0800

    feat: upgrade react, radix, tw4

--- template/fe-template-c/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import { preset } from "@civictechmy/myds-style";
-
-export default {
-  content: [
-    "src/**/*.{js,jsx,ts,tsx}", // Your own project source files
-    "node_modules/@civictechmy/myds-react/dist/**/*.{js,jsx,ts,tsx}",
-  ],
-  presets: [preset],
-};
